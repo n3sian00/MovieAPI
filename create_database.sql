f33a110a981d2c6653d3bfe710359d936b20a04d
n3sian00 <n3sian00@students.oamk.fi>
2024-11-14 15:34:50 +0200

    Korjauksia

--- create_database.sql
+++ create_database.sql
@@ -4,26 +4,37 @@ CREATE TABLE genres (
     descr VARCHAR(255)
 );
 
-CREATE TABLE studio (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    address VARCHAR(255)
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(255),
+    year_of_birth INT
 );
 
-CREATE TABLE director (
+CREATE TABLE favorite (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255)
+    user_id INT,
+    movie_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
 );
+
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    movie_id INT,
+    stars INT CHECK (stars >= 1 AND stars <= 5), -- Rating between 1 and 5 stars
+    review_txt TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
+
 
 CREATE TABLE movie (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     year INT,
-    genres VARCHAR(255),
-    studio_id INT,
-    director_id INT,
-    FOREIGN KEY (genres) REFERENCES genres(name),
-    FOREIGN KEY (studio_id) REFERENCES studio(id),
-    FOREIGN KEY (director_id) REFERENCES director(id)
+    genre_name VARCHAR(255),
+    FOREIGN KEY (genre_name) REFERENCES genres(name)
 );
