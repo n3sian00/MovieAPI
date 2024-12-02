32f1572245fa87d95914d388294af6b8e267c658
n3sian00 <n3sian00@students.oamk.fi>
2024-12-02 20:57:05 +0200

    DB connection

--- serverendpoints.js
+++ serverendpoints.js
@@ -1,16 +1,32 @@
 import express from 'express';
+import pkg from 'pg'; 
+
+const { Pool } = pkg; 
 
 const app = express();
-
 app.use(express.json());
 
 app.listen(3001, () => {
     console.log('Server is running on http://localhost:3001');
 });
 
+//
+
+const pool = new Pool({
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'postgres',
+    password: 'Tammikuu2000',
+    port: 5432,
+});
+
+//
+
 app.get('/', (req, res) => {
     res.send('You just called root endpoint');
 });
+
+//
 
 app.post('/genres', (req, res) => {
     res.status(201).send({ message: 'New genre added', data: req.body });
